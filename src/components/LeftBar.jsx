c1d4b4f98616011077da3dd17eda77e845336229
Najib Alejandro <alejandrorod1498@gmail.com>
2025-06-21 12:25:00 -0600

    left bar desing

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import SimpleCardComp from './SimpleCard';
 
 function LeftBar({title}) {
     return ( 
-        <div className='w-1/4 h-full bg-blue-500' >
-            <h1>Here listado </h1>
+        <div className='flex w-1/4 h-full from-gray-900 flex-col' >
+           <br />
+           <div className='flex p-3 justify-center h-auto w-auto'>
+           <img src="/src/assets/logo.png"   />
+           </div>
+          <br />
+          <div className=' m-4'>
+            <SimpleCardComp title="Home"/>
+            <SimpleCardComp title="About"/>
+            <SimpleCardComp title="Dashboard"/>
+            <SimpleCardComp title="Contact"/>
+            <SimpleCardComp title="Social"/>
+          </div>
+
+          <div className=" flex-1 flex items-end justify-around p-6">
+          <i className="fab fa-facebook-f text-white text-xl hover:text-blue-500 transition duration-200" />
+    <i className="fab fa-twitter text-white text-xl hover:text-sky-400 transition duration-200" />
+    <i className="fab fa-instagram text-white text-xl hover:text-pink-500 transition duration-200" />
+    <i className="fab fa-linkedin-in text-white text-xl hover:text-blue-400 transition duration-200" />
+        </div>
         </div>
     );
 }
---
+++ src/components/SimpleCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function SimpleCardComp({title}) {
+    return ( 
+     
+   <div className="p-4 m-3 rounded-lg bg-gradient-to-r
+    from-gray-900 via-gray-800 to-black
+     text-white shadow-sm shadow-amber-50
+        transform hover:scale-105 transition duration-300 ease-in-out
+  cursor-pointer
+     ">
+        <h2 className="text-lg font-semibold text-center">{title}</h2>
+   </div>
+     );
+}
+
+export default SimpleCardComp;
